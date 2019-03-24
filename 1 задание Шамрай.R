5c3458fb58fb7d61d4b36b645dcbae7f2edeb40d
shamraielizaveta <48158775+shamraielizaveta@users.noreply.github.com>
2019-03-24 20:05:04 +0300

    Add files via upload

---
+++ 1 задание Шамрай.R
@@ -0,0 +1,55 @@
+library(tidyverse)
+library(rnoaa)
+library(lubridate)
+
+#Данные для расчета (так как di не нулевой только с 4 по 8 месяц, то оставим только нужные значения):
+ai = c(32.11, 26.31, 25.64, 23.20, 18.73)
+bi = c(11.30, 9.26, 9.03, 8.16, 6.59)
+#di вычисляем самостоятельно ниже
+Kf = 300 #  Коэффициент использования ФАР
+Qj = 1600 # калорийность урожая культуры
+Lj = 2.2 #  сумма частей основной и побочной продукции
+Ej = 25 #   стандартная влажность культуры
+
+#station_data = ghcnd_stations() #Может занять несколько минут лучше выполнить один раз в месте с хорошим интернетом и сохранить результат
+#write.csv(station_data, file = "station_data.csv")
+station_data = read.csv("station_data.csv") [,-1]
+
+#После получения списка всех станций, получите список станций ближайших к столице вашего региона,создав таблицу с именем региона и координатами его столицы
+Volgograd = data.frame(id = "Volgograd", latitude = 48.699167,  longitude = 44.473333)
+Volgograd_around = meteo_nearby_stations(lat_lon_df = Volgograd, station_data = station_data,
+                                    limit = 3, var = c("PRCP", "TAVG"),
+                                    year_min = 2009, year_max = 2009)
+#Volgograd_around это список единственным элементом которого является таблица, содержащая идентификаторы метеостанций отсортированных по их 
+# удалленности от Volgograda, очевидно что первым элементом таблицы будет идентификатор метеостанции Volgograda, его то мы и попытаемся получить
+Volgograd_id = Volgograd_around[["Volgograd"]][["id"]][1]
+#данные за 2009 год есть только на одной из трех метеостанций вблизи г. Волгоград, цикл не требуется
+
+#Для получения всех данных с метеостанции, зная ее идентификатор, используйте след. команду
+all_Volgograd_data = meteo_tidy_ghcnd(stationid = Volgograd_id, date_min="2009-04-1",date_max="2009-8-31")
+
+#оставляем интересующие нас столбики
+all_Volgograd_data = select(all_Volgograd_data, id, date, tavg)
+
+#преобразуем дату в месяц и день и добавим столбики
+all_Volgograd_data = mutate(all_Volgograd_data, month = month(date), day = day(date))
+all_Volgograd_data_without0 = all_Volgograd_data #сохраним резеврную таблицу без обнуления
+  
+#обнулим значение температуры в невегетативный период
+all_Volgograd_data[(all_Volgograd_data$month == 4 & all_Volgograd_data$day <= 14),"tavg"] = 0
+all_Volgograd_data[(all_Volgograd_data$month == 8 & all_Volgograd_data$day >= 16),"tavg"] = 0
+
+#Сгруппируем по месяцам
+all_Volgograd_data = all_Volgograd_data %>% group_by(month)
+all_Volgograd_data_without0 = all_Volgograd_data_without0 %>% group_by(month)
+
+#Вычислим di для каждого месяца
+di = summarize(all_Volgograd_data, di = length(tavg[tavg>70])/length(tavg))[,-1]
+
+#Вычислим сумму температур больше 5 градусов в каждом месяце
+St = summarize(all_Volgograd_data_without0, St = sum(tavg[tavg>50])/10)[,-1]
+
+#Найдем урожаность по формуле:
+Fi = ai + bi * 1.0 * St
+yield = 10^6*sum(Fi*di*Kf/(Qj*Lj*(100-Ej)))
+yield
